401a028c3ac63fa8b70483d27b43382a6e6ca2c1
devBustamento <dev.bustamento@gmail.com>
2018-05-14 17:27:43 -0500

    Changed App settings and did Dapper stuff

---
+++ AspNetCoreMvc/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvc.Models;
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var people = new List<Person>
+            {
+                new Person { Name = "WHAT" },
+                new Person { Name = "THE HELL" },
+                new Person { Name = "IS" },
+                new Person { Name = "REFLECTION" },
+                new Person { Name = "IoC/DI" },
+                new Person { Name = "MVC" },
+                new Person { Name = "PLZ SEND HLP" }
+            };   
+           
+            return View(people);
+        }
+    }
+}
---
+++ Itb.Repositories/ProductRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Itb.Shared;
+
+namespace Itb.Repositories 
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn; 
+        }
+
+        public int CreateProduct(Product prod)
+        {
+            using (var conn = _conn)
+            {
+                conn.Open();
+                return conn.Execute("INSTER INTO product (Name) VALUES (@Name)", prod);
+            }
+        }
+
+        public int DeleteProduct(int id)
+        {
+            using (var conn = _conn)
+            {
+                conn.Open();
+                return conn.Execute("DELETE FROM product WHERE ProductId = @Id", new { id });
+            }
+        }
+
+        public int UpdateProduct(Product prod)
+        {
+            using (var conn = _conn)
+            {
+                conn.Open();
+                return conn.Execute("UPDAGE product SET Name = @Name WHERE ProductId = @id", prod);
+            }
+        }
+
+        public Product GetProduct(int id)
+        {
+            using (var conn = _conn)
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT *, ProductId FROM Product as Id WHERE ProductId = @Id").FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var conn = _conn)
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT *, ProductId FROM Product as Id WHERE ProductId = @Id");
+            }
+        }
+    }
+}
